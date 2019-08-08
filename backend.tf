8f0cf5da0925b041074794f5c0b84873e5a63bad
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-07 17:13:04 -0700

    fixed

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform{
-        backend ="s3" 
-        region = "us-east-1"
-        bucket =  "remote-exec-mustafa"
-        key  = "remote.state"
-    
+terraform {
+    backend "s3" {
+        region  =  "us-east-1"
+        bucket = "remote-exec-mustafa"
+        key = "remote.state"
+    }
 }
