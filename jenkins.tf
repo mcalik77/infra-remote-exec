cd204dacde8dcec7e07eab20ae50fa408ccc9c0f
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-07 17:31:23 -0700

    edit

--- jenkins.tf
+++ jenkins.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "jenkins" {
             private_key = "${file("~/.ssh/id_rsa")}"
         }
         inline  = [
-            "sudo yum install java-1.8.0-openjdk-devel",
+            "sudo yum install java-1.8.0-openjdk-devel -y",
             "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
             "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
             "sudo yum install jenkins -y",
