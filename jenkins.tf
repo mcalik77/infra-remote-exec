45fd2b25216370b9a92506be9f2f97d9344f129f
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-07 17:33:50 -0700

    edited

--- jenkins.tf
+++ jenkins.tf
@@ -17,7 +17,8 @@ resource "aws_instance" "jenkins" {
             "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
             "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
             "sudo yum install jenkins -y",
-            "sudo systemctl start jenkins"
+            "sudo systemctl start jenkins",
+            "sudo systemctl enable jenkins"
         ]
     }
 }
