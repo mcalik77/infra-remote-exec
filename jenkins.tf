6560f9cc4f97bb6147f71effbd7607d8f2339d8a
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-07 17:27:03 -0700

    edit

--- jenkins.tf
+++ jenkins.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkins" {
     instance_type = "${var.instance_type}"
     key_name =  "${aws_key_pair.developer_key.id}"
     associate_public_ip_address = true
-    security_groups = ["${aws_security_group.jenkins.id}"]
+    security_groups = ["${aws_security_group.jenkins.name}"]
 
     provisioner "remote-exec" {
         connection {
